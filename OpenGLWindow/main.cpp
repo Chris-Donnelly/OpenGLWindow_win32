80ef09e4a43e7a319069dc2cb91a001570a8b507
Chris Donnelly <christdonnelly-phone@hotmail.co.uk>
2022-01-10 20:38:15 +0100

    Changed window title

--- OpenGLWindow/main.cpp
+++ OpenGLWindow/main.cpp
@@ -7,7 +7,7 @@ void MainLogicCB(float delta)
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
     // Start renderer window
-    COpenGLWindow window(200, 200, 800, 400, L"RemoteGIMX Test app", MainLogicCB);
+    COpenGLWindow window(200, 200, 800, 400, L"Window app", MainLogicCB);
     window.Run();
 
     return 0;
